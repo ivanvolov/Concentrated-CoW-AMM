4b80db58625cca6f2ddb9bc70b71c44ff2e32cdc
ivan <vivan.volovik@gmail.com>
2024-08-18 18:55:55 +0200

    +DeterministicDeployment

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 ta:
 	clear && forge test -vvv --no-match-test "fork|[fF]uzz"
 t:
-	clear && forge test -vvv --match-contract CreateAMM --match-test "test"
+	clear && forge test -vvv --match-contract DeterministicDeployment --match-test "testDifferentOwnersCanDeployAMMWithSameParameters"
--- test/CConstantProductFactory/Create/DeterministicDeployment.sol
+++ test/CConstantProductFactory/Create/DeterministicDeployment.sol
@@ -5,158 +5,207 @@ import {ICPriceOracle, CConstantProduct, IERC20} from "src/CConstantProductFacto
 
 import {CConstantProductFactoryTestHarness} from "../CConstantProductFactoryTestHarness.sol";
 
-//TODO: uncomment
+import {V3MathLib} from "src/libraries/V3MathLib.sol";
+
 abstract contract DeterministicDeployment is
     CConstantProductFactoryTestHarness
 {
-    // uint256 private amount0 = 1234;
-    // uint256 private amount1 = 5678;
-    // uint256 private minTradedToken0 = 42;
-    // ICPriceOracle private priceOracle =
-    //     ICPriceOracle(makeAddr("Create: price oracle"));
-    // bytes private priceOracleData = bytes("some price oracle data");
-    // bytes32 private appData = keccak256("Create: app data");
-    // // function testDeploysAtExpectedAddress() public {
-    // //     address constantProductAddress = constantProductFactory
-    // //         .ammDeterministicAddress(
-    // //             address(this),
-    // //             mockableToken0,
-    // //             mockableToken1
-    // //         );
-    // //     require(
-    // //         constantProductAddress.code.length == 0,
-    // //         "no AMM should be deployed at the start"
-    // //     );
-    // //     mocksForTokenCreation(constantProductAddress);
-    // //     CConstantProduct deployed = constantProductFactory.create(
-    // //         mockableToken0,
-    // //         amount0,
-    // //         mockableToken1,
-    // //         amount1,
-    // //         minTradedToken0,
-    // //         priceOracle,
-    // //         priceOracleData,
-    // //         appData
-    // //     );
-    // //     assertEq(address(deployed), constantProductAddress);
-    // //     assertTrue(constantProductAddress.code.length > 0);
-    // // }
-    // // function testSameOwnerCannotDeployAMMWithSameParametersTwice() public {
-    // //     address constantProductAddress = constantProductFactory
-    // //         .ammDeterministicAddress(
-    // //             address(this),
-    // //             mockableToken0,
-    // //             mockableToken1
-    // //         );
-    // //     mocksForTokenCreation(constantProductAddress);
-    // //     constantProductFactory.create(
-    // //         mockableToken0,
-    // //         amount0,
-    // //         mockableToken1,
-    // //         amount1,
-    // //         minTradedToken0,
-    // //         priceOracle,
-    // //         priceOracleData,
-    // //         appData
-    // //     );
-    // //     vm.expectRevert(bytes(""));
-    // //     constantProductFactory.create(
-    // //         mockableToken0,
-    // //         amount0,
-    // //         mockableToken1,
-    // //         amount1,
-    // //         minTradedToken0,
-    // //         priceOracle,
-    // //         priceOracleData,
-    // //         appData
-    // //     );
-    // // }
-    // function testSameOwnerCanDeployAMMWithDifferentTokens() public {
-    //     address ammAddress1 = constantProductFactory.ammDeterministicAddress(
-    //         address(this),
-    //         mockableToken0,
-    //         mockableToken1
-    //     );
-    //     mocksForTokenCreation(ammAddress1);
-    //     // Same setup as in `mocksForTokenCreation`, but for newly created tokens.
-    //     IERC20 extraToken0 = IERC20(
-    //         makeAddr("DeterministicDeployment: extra token 0")
-    //     );
-    //     IERC20 extraToken1 = IERC20(
-    //         makeAddr("DeterministicDeployment: extra token 1")
-    //     );
-    //     address ammAddress2 = constantProductFactory.ammDeterministicAddress(
-    //         address(this),
-    //         extraToken0,
-    //         extraToken1
-    //     );
-    //     setUpTokenForDeployment(
-    //         extraToken0,
-    //         ammAddress2,
-    //         address(constantProductFactory)
-    //     );
-    //     setUpTokenForDeployment(
-    //         extraToken1,
-    //         ammAddress2,
-    //         address(constantProductFactory)
-    //     );
-    //     constantProductFactory.create(
-    //         mockableToken0,
-    //         amount0,
-    //         mockableToken1,
-    //         amount1,
-    //         minTradedToken0,
-    //         priceOracle,
-    //         priceOracleData,
-    //         appData
-    //     );
-    //     constantProductFactory.create(
-    //         extraToken0,
-    //         amount0,
-    //         extraToken1,
-    //         amount1,
-    //         minTradedToken0,
-    //         priceOracle,
-    //         priceOracleData,
-    //         appData
-    //     );
-    // }
-    // function testDifferentOwnersCanDeployAMMWithSameParameters() public {
-    //     address owner1 = makeAddr("DeterministicDeployment: owner 1");
-    //     address owner2 = makeAddr("DeterministicDeployment: owner 2");
-    //     address ammOwner1 = constantProductFactory.ammDeterministicAddress(
-    //         owner1,
-    //         mockableToken0,
-    //         mockableToken1
-    //     );
-    //     address ammOwner2 = constantProductFactory.ammDeterministicAddress(
-    //         owner2,
-    //         mockableToken0,
-    //         mockableToken1
-    //     );
-    //     mocksForTokenCreation(ammOwner1);
-    //     mocksForTokenCreation(ammOwner2);
-    //     vm.prank(owner1);
-    //     constantProductFactory.create(
-    //         mockableToken0,
-    //         amount0,
-    //         mockableToken1,
-    //         amount1,
-    //         minTradedToken0,
-    //         priceOracle,
-    //         priceOracleData,
-    //         appData
-    //     );
-    //     vm.prank(owner2);
-    //     constantProductFactory.create(
-    //         mockableToken0,
-    //         amount0,
-    //         mockableToken1,
-    //         amount1,
-    //         minTradedToken0,
-    //         priceOracle,
-    //         priceOracleData,
-    //         appData
-    //     );
-    // }
+    uint256 private amount0 = 1999508296761490795;
+    uint256 private amount1 = 220271565651919101572;
+    uint160 private DEFAULT_PRICE_UPPER_X96 =
+        V3MathLib.getSqrtPriceFromPrice(5500 ether);
+    uint160 private DEFAULT_PRICE_LOWER_X96 =
+        V3MathLib.getSqrtPriceFromPrice(4545 ether);
+    uint128 private DEFAULT_LIQUIDITY = 1518129116516325613903;
+
+    uint256 private minTradedToken0 = 42;
+    ICPriceOracle private priceOracle =
+        ICPriceOracle(makeAddr("Create: price oracle"));
+    bytes32 private appData = keccak256("Create: app data");
+
+    function testDeploysAtExpectedAddress() public {
+        address constantProductAddress = constantProductFactory
+            .ammDeterministicAddress(
+                address(this),
+                mockableToken0,
+                mockableToken1
+            );
+        require(
+            constantProductAddress.code.length == 0,
+            "no AMM should be deployed at the start"
+        );
+        mocksForTokenCreation(constantProductAddress);
+        setUpOracleResponse(
+            DEFAULT_NEW_PRICE_X96,
+            address(priceOracle),
+            address(mockableToken0),
+            address(mockableToken1)
+        );
+        CConstantProduct deployed = constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+        assertEq(address(deployed), constantProductAddress);
+        assertTrue(constantProductAddress.code.length > 0);
+    }
+
+    function testSameOwnerCannotDeployAMMWithSameParametersTwice() public {
+        address constantProductAddress = constantProductFactory
+            .ammDeterministicAddress(
+                address(this),
+                mockableToken0,
+                mockableToken1
+            );
+        mocksForTokenCreation(constantProductAddress);
+        setUpOracleResponse(
+            DEFAULT_NEW_PRICE_X96,
+            address(priceOracle),
+            address(mockableToken0),
+            address(mockableToken1)
+        );
+        constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+        vm.expectRevert(bytes(""));
+        constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+    }
+
+    function testSameOwnerCanDeployAMMWithDifferentTokens() public {
+        address ammAddress1 = constantProductFactory.ammDeterministicAddress(
+            address(this),
+            mockableToken0,
+            mockableToken1
+        );
+        mocksForTokenCreation(ammAddress1);
+        setUpOracleResponse(
+            DEFAULT_NEW_PRICE_X96,
+            address(priceOracle),
+            address(mockableToken0),
+            address(mockableToken1)
+        );
+        // Same setup as in `mocksForTokenCreation`, but for newly created tokens.
+        IERC20 extraToken0 = IERC20(
+            makeAddr("DeterministicDeployment: extra token 0")
+        );
+        IERC20 extraToken1 = IERC20(
+            makeAddr("DeterministicDeployment: extra token 1")
+        );
+        address ammAddress2 = constantProductFactory.ammDeterministicAddress(
+            address(this),
+            extraToken0,
+            extraToken1
+        );
+        setUpTokenForDeployment(
+            extraToken0,
+            ammAddress2,
+            address(constantProductFactory)
+        );
+        setUpTokenForDeployment(
+            extraToken1,
+            ammAddress2,
+            address(constantProductFactory)
+        );
+        setUpOracleResponse(
+            DEFAULT_NEW_PRICE_X96,
+            address(priceOracle),
+            address(extraToken0),
+            address(extraToken1)
+        );
+
+        constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+
+        constantProductFactory.create(
+            extraToken0,
+            extraToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+    }
+
+    function testDifferentOwnersCanDeployAMMWithSameParameters() public {
+        address owner1 = makeAddr("DeterministicDeployment: owner 1");
+        address owner2 = makeAddr("DeterministicDeployment: owner 2");
+        address ammOwner1 = constantProductFactory.ammDeterministicAddress(
+            owner1,
+            mockableToken0,
+            mockableToken1
+        );
+        address ammOwner2 = constantProductFactory.ammDeterministicAddress(
+            owner2,
+            mockableToken0,
+            mockableToken1
+        );
+        mocksForTokenCreation(ammOwner1);
+        mocksForTokenCreation(ammOwner2);
+        setUpOracleResponse(
+            DEFAULT_NEW_PRICE_X96,
+            address(priceOracle),
+            address(mockableToken0),
+            address(mockableToken1)
+        );
+        vm.prank(owner1);
+        constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+        vm.prank(owner2);
+        constantProductFactory.create(
+            mockableToken0,
+            mockableToken1,
+            DEFAULT_LIQUIDITY,
+            minTradedToken0,
+            priceOracle,
+            DEFAULT_PRICE_ORACLE_DATA,
+            appData,
+            DEFAULT_PRICE_UPPER_X96,
+            DEFAULT_PRICE_LOWER_X96
+        );
+    }
 }
