10b0123d7ece02f69ee09a2fc0d28c7bb1ed3180
ivan <vivan.volovik@gmail.com>
2024-09-24 11:08:24 +0200

    +EnableTrading

--- test/CConstantProduct/EnableTrading.sol
+++ test/CConstantProduct/EnableTrading.sol
@@ -5,33 +5,44 @@ import {CConstantProductTestHarness, CConstantProduct} from "./CConstantProductT
 import {TradingParams} from "src/interfaces/ICConstantProduct.sol";
 
 abstract contract EnableTrading is CConstantProductTestHarness {
-    // function testEnableTradingDoesNotRevert() public {
-    //     TradingParams memory defaultTradingParams = getDefaultTradingParams();
-    //     constantProduct.enableTrading(defaultTradingParams);
-    // }
-    // function testEnableTradingRevertsIfCalledByNonManager() public {
-    //     TradingParams memory defaultTradingParams = getDefaultTradingParams();
-    //     vm.prank(makeAddr("this is not the owner"));
-    //     vm.expectRevert(
-    //         abi.encodeWithSelector(CConstantProduct.OnlyManagerCanCall.selector)
-    //     );
-    //     constantProduct.enableTrading(defaultTradingParams);
-    // }
-    // function testEnableTradingEmitsEvent() public {
-    //     TradingParams memory defaultTradingParams = getDefaultTradingParams();
-    //     vm.expectEmit();
-    //     emit CConstantProduct.TradingEnabled(
-    //         constantProduct.hash(defaultTradingParams),
-    //         defaultTradingParams
-    //     );
-    //     constantProduct.enableTrading(defaultTradingParams);
-    // }
-    // function testEnableTradingSetsState() public {
-    //     TradingParams memory defaultTradingParams = getDefaultTradingParams();
-    //     constantProduct.enableTrading(defaultTradingParams);
-    //     assertEq(
-    //         constantProduct.tradingParamsHash(),
-    //         constantProduct.hash(defaultTradingParams)
-    //     );
-    // }
+    function testEnableTradingDoesNotRevert() public {
+        setUpDefaultReserves(address(constantProduct));
+        setUpDefaultDeposit();
+        TradingParams memory defaultTradingParams = getDefaultTradingParams();
+        constantProduct.enableTrading(defaultTradingParams);
+    }
+
+    function testEnableTradingRevertsIfCalledByNonManager() public {
+        setUpDefaultReserves(address(constantProduct));
+        setUpDefaultDeposit();
+        TradingParams memory defaultTradingParams = getDefaultTradingParams();
+        vm.prank(makeAddr("this is not the owner"));
+        vm.expectRevert(
+            abi.encodeWithSelector(CConstantProduct.OnlyManagerCanCall.selector)
+        );
+        constantProduct.enableTrading(defaultTradingParams);
+    }
+
+    function testEnableTradingEmitsEvent() public {
+        setUpDefaultReserves(address(constantProduct));
+        setUpDefaultDeposit();
+        TradingParams memory defaultTradingParams = getDefaultTradingParams();
+        vm.expectEmit();
+        emit CConstantProduct.TradingEnabled(
+            constantProduct.hash(defaultTradingParams),
+            defaultTradingParams
+        );
+        constantProduct.enableTrading(defaultTradingParams);
+    }
+
+    function testEnableTradingSetsState() public {
+        setUpDefaultReserves(address(constantProduct));
+        setUpDefaultDeposit();
+        TradingParams memory defaultTradingParams = getDefaultTradingParams();
+        constantProduct.enableTrading(defaultTradingParams);
+        assertEq(
+            constantProduct.tradingParamsHash(),
+            constantProduct.hash(defaultTradingParams)
+        );
+    }
 }
